886bc2b3e8a909b19c30c1c494e3b86e68a0457b
YaCpotato <yasshisshy@gmail.com>
2019-08-24 13:15:46 +0900

    adminにて画像アップロードを確認

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Book,Review,Image
+
+admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(Image)
