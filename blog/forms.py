68874010c8015699c6b890aaf8c2a361d25dabcf
YaCpotato <yasshisshy@gmail.com>
2019-08-31 20:18:01 +0900

    star機能を追加

--- blog/forms.py
+++ blog/forms.py
@@ -6,10 +6,10 @@ class BookForm(forms.ModelForm):
 
     class Meta:
         model = Book
-        fields = ('title', 'link','image')
+        fields = ('author','title', 'link','image')
 
 class ReviewForm(forms.ModelForm):
 
     class Meta:
         model = Review
-        fields = ('title', 'content','star')
+        fields = ('author','title', 'content','star')
